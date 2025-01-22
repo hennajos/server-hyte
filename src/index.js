ffb0a49b0f00de45a0f6343bce8273adf9f1180f
Henna <142875041+hennajos@users.noreply.github.com>
2025-01-22 15:09:08 +0200

    viikko 1 harjoitus

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import http from 'http';
+import express from 'express';
 const hostname = '127.0.0.1';
+const app = express();
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-  console.log("Uusi http-pyyntö");
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Welcome to my REST API!');
+app.get('/api/', (req, res) => {
+  res.send('Welcome to my REST API!');
 });
 
-server.listen(port, hostname, () => {
+app.get('/api/sum/:num1/:num2', (req, res) => {
+  console.log(req.params);
+  const num1 = Number(req.params.num1);
+  const num2 = Number(req.params.num2);
+  if(isNaN(num1) || isNaN(num2)) {
+    res.status(400);
+    res.json({
+      error: 'Molempien parametrien pitää olla numeroita.'
+    });
+    return;
+  }
+  res.json({Summa: num1 + num2});
+});
+
+app.post('/api/moro', (req, res) => {
+  console.log(req.body);
+  res.status(201);
+  res.json({reply: 'Tervetuloa sovellukseen ' + req.body.sender});
+});
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
